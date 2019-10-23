413e50571ba8e50bd8f9944492e70f4a084984c2
Darrien Gunn <dgunn001@wch136-06.cs.ucr.edu>
2019-10-23 02:25:43 -0700

    last push

--- Lab5_Hardware/source/main.c
+++ Lab5_Hardware/source/main.c
@@ -1,87 +1,96 @@
-/*	Author: dgunn001
-*  Partner(s) Name:
-*	Lab Section:
-*	Assignment: Lab #  Exercise #
-*	Exercise Description: [optional - include for your own benefit]
-*
-*	I acknowledge all content contained herein, excluding template or example
-*	code, is my own original work.
-*/
 
 
+rtner(s) Name: 
+ *	Lab Section:
+ *	 *	Assignment: Lab #  Exercise #
+ *	  *	Exercise Description: [optional - include for your own benefit]
+ *	   *
+ *	    *	I acknowledge all content contained herein, excluding template or example
+ *	     *	code, is my own original work.
+ *	      */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum States{start,wait, trans} state;
+
+unsigned char button;
+unsigned char button2;
+
+unsigned char tempC;
+
 
 int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    tempC = 0x07;
+    state = start;
+    /* Insert your solution below */
+    while (1) {
+	button = PINA & 0x01;
+	button2 = PINA & 0x02;
 
-   /* Insert DDR and PORT initializations */
-   DDRA = 0x00; PORTA = 0xFF;
-   DDRC = 0xFF; PORTC = 0x00;
-   unsigned char tmpC  = 0x00;
-   unsigned char tmpB = 0x00;
-   unsigned char tmpA = 0x00;
-
-
-   //unsigned char tmpB = 0x00; used for challenge
-
-
-	/* Insert your solution below */
-   while (1) {
-	tmpA = PINA & 0x0F;
-	
-	switch(tmpA){
-		case 1:
-		case 2:
-		   tmpC = 0x20;
-		   tmpC = tmpC | 0x40;
-
+	switch(state){
+	    case start:
+		tempC = 0x07; 
+		if(button && !button2){
+			if(tempC < 9){
+			   tempC++;
+			   state = wait;
+			} else {
+			   state = wait;
+			}
+		} else if (!button && button2) {
+			if(tempC > 0){
+			   tempC--;
+			   state = wait;
+			} else {
+			   state = wait;
+			}
+		} else if (button && button2){
+			tempC = 0;
+			state = wait;
+		} else {
+			state = trans;
+		}
 		break;
-		case 3:
-		case 4:
-		   tmpC = 0x30;
-		   tmpC = tmpC | 0x40;
-
+	    case trans: 
+		if(button && !button2){
+			if(tempC < 9){
+			   tempC++;
+			   state = wait;
+			} else {
+			   state = wait;
+			}
+		} else if (!button && button2) {
+			if(tempC > 0){
+			   tempC--;
+			   state = wait;
+			} else {
+			   state = wait;
+			}
+		} else if (button && button2){
+			tempC = 0;
+			state = wait;
+		} else {
+			state = trans;
+		}
 		break;
-		case 5:
-		case 6:
-		   tmpC = 0x38;
-
+	    case wait:
+		if(!button && !button2){
+			state = trans;
+		} else if(button && button2){
+			tempC = 0;
+			state = wait;
+		} else {
+			state = wait;
+		}
 		break;
-		case 7:
-		case 8:
-		case 9:
-		   tmpC = 0x3C;
-
-		break;
-		case 10:
-		case 11:
-		case 12:
-		   tmpC = 0x3E;
-
-		break;
-		case 13:
-		case 14:
-		case 15:
-		   tmpC = 0x3F;
-
-		break;
-
-
 	}
-	tmpB = PINA & 0xF0;
-	if(tmpB == 0x30){
-		tmpC = tmpC | 0x80;
-	}
-   	PORTC = tmpC;
-
-
-   }
-
-
-   return 1;
-
-
+	PORTC = tempC;
+    }
+    return 1;
 }
+
