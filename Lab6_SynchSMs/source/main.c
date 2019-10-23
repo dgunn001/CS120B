9ecc51dfda898defc63e7fd622e8fbf4c59b8e31
Darrien Gunn <dgunn001@wch136-06.cs.ucr.edu>
2019-10-23 02:38:28 -0700

    part 1

--- Lab6_SynchSMs/source/main.c
+++ Lab6_SynchSMs/source/main.c
@@ -8,16 +8,93 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+#include <avr/interrupt.h>
+
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x08;
+}
+
+void TimerOff(){
+	TCCR1B = 0x00;
+}
+
+void TimerISR(){
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0) {
+	TimerISR();
+	_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+void TimerSet(unsigned long M){
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+unsigned char tmpC = 0x00;
+enum States{init, light1, light2 , light3}state;
+
+void cycle3(){
+	switch(state){
+	case init:
+	state = light1;
+	break;
+	case light1:
+	state = light2;
+	break;
+	case light2:
+	state = light3;
+	break;
+	case light3:
+	state = light1;
+	break;
+	}
+	switch(state){
+	case light1:
+	tmpC = 0x01;
+	break;
+	case light2:
+	tmpC = 0x02;
+	break;
+	case light3:
+	tmpC = 0x04;
+	break;
+	}
+}
+	
+	
+
+
+	
+
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRC = 0xFF; PORTC = 0x00;
     /* Insert your solution below */
+	TimerSet(1000);
+	TimerOn();
+	state = init;
+	
     while (1) {
-
+	lightTick();
+	while(!TimerFlag);
+	TimerFlag = 0;
+	PORTC = tmpC;
     }
-    return 1;
+    return 0;
 }
