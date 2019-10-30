ba97b52835c767a7950133dfd4c9b70ca413e8c4
Darrien Gunn <dgunn001@wch136-10.cs.ucr.edu>
2019-10-30 01:35:18 -0700

    lab8 part 3

---
+++ Lab8_ADC/turnin/dgunn001_lab7_part3.c
@@ -0,0 +1,37 @@
+/*	Author: dgunn001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1<< ADSC) | (1<<ADATE);
+}
+
+int main(void){
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+
+	ADC_init();
+	unsigned short x = 0x00;
+	unsigned char tempB = 0x00;
+	unsigned char tempD = 0x00
+	while(1){
+		x = ADC;
+		tempB = (char)x;
+		tempD = (char)(x >> 8);
+		if(photo < MAX/2){
+			PORTB = 0x00;
+		} else {
+			PORTB = 0x01;
+		}
+	}
+}
