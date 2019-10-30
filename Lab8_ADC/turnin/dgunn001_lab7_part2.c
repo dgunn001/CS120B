bd34664472ff40d911305761008add4d6ef5eef0
Darrien Gunn <dgunn001@wch136-10.cs.ucr.edu>
2019-10-30 01:29:08 -0700

    lab8 part1

---
+++ Lab8_ADC/turnin/dgunn001_lab7_part2.c
@@ -0,0 +1,31 @@
+/*	Author: dgunn001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN) | (1<< ADSC) | (1<<ADATE);
+}
+
+int main(void){
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+
+	ADC_init();
+	while(1){
+		unsigned short x = ~ADC;
+		unsigned char bPort = (char)x;
+		PORTB = aPort;
+		unsigned char dPort = (char)(x >> 8);
+		PORTD = dPort;
+	}
+}
